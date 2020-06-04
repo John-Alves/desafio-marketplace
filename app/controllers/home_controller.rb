69b5b50756a1dab72ef7110dd92ce6803e45aaf8
John-Alves <johnnydealves99@gmail.com>
2020-06-04 14:26:42 -0300

    Improving layout structure and styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ActionController::Base
   layout 'unauthenticated'
   def index
-    @products = Product.search("*#{params[:query]}*").records.records
+    search_result = Product.search("*#{params[:query]}*")
+    @products = search_result.records.records
+    @quantity = search_result.count
   end
 end
--- app/javascript/packs/unauthenticated.js
+++ app/javascript/packs/unauthenticated.js
@@ -2,4 +2,18 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("channels");
 global.jQuery, global.$ = require("jquery");
-import 'materialize-css/dist/js/materialize';
+
+document.addEventListener('turbolinks:load', () => {
+    let navs = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(navs, {});
+
+    let dropdowns = document.querySelectorAll(".dropdown-trigger");
+    M.Dropdown.init(dropdowns, {});
+
+    let flashMessages = document.querySelectorAll(".flash-messages");
+    flashMessages.forEach((element, index) => {
+        if (element.textContent){
+            M.toast({html: element.textContent, displayLength: 2000})
+        }
+    })
+});
