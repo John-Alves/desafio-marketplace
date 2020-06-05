a87afa36c490054b0c274665e6672d60f514c34e
John-Alves <johnnydealves99@gmail.com>
2020-06-05 01:02:52 -0300

    Product page style finished

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 class HomeController < ActionController::Base
   layout 'marketplace'
+  before_action :get_stores, only: [:index]
+
   def index
     search_result = Product.search("*#{params[:query]}*")
     @products = search_result.records.records
     @quantity = search_result.count
+  end
+
+  private
+
+  def get_stores
+    @stores = Store.search("*").records.records
   end
 end
